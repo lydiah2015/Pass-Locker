77ab5fc60ff66f518c0f67ddae1e954b0192bd40
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-04-06 17:08:37 +0300

    created a credentials file with two classes

---
+++ credentials.py
@@ -0,0 +1,28 @@
+class User:
+    """
+    class that holds user details
+    """ 
+    user_list = [] 
+
+    def __init__(self,first_name,last_name,username,password):
+
+        """
+        class that generates new intances of contacts
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+        
+    def save_user(self):
+        """
+        method to save user's details
+        """
+        User.user_list.append(self)
+
+    def delete_user(self):
+        """
+        deletes user details from the array that has been saved
+        """
+        User.user_list.remove(self)
