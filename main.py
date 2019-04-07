8aed3c5e8adba30f8cbee6d99d911e5d682950bd
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-04-08 02:52:09 +0300

    implemented create new account option with random password

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+import string
+import random
 from user import User
 
 def print_message_head(message):
@@ -20,12 +21,33 @@ def select_option():
     return option
 
 
+# option 1
 def add_existing_account():
     name=input("Account name:")
     login=input("Login: ")
     password=input("Password: ")
     user.credential.add_credential(name,login,password)
-    
+    input("Done, Press Enter to Continue")
+
+
+# option 2 
+def add_new_account():
+    name=input("Account name:")
+    login=input("Login: ")
+    print("\nChose an option:\n")
+    print("1)Set password.\n2)Generate password.\n")
+    option=int(input("Option: "))
+    if option == 1:
+        password=input("Password: ")
+    elif option == 2:
+        pass_length=int(input("Password Length: "))
+        password="".join(random.choices(string.ascii_letters+string.digits,k=pass_length))
+    else:
+        print("Invalid option!!")
+        return 
+    user.credential.add_credential(name,login,password)
+    input("Done, Press Enter to Continue")
+ 
 
 def list_credentials():
     print("#"*50)
@@ -34,6 +56,7 @@ def list_credentials():
             "Account Name: %s    Login: %s    Password: %s"%(cred["name"],cred["login"],cred["password"])
         )
     print("#"*50)
+    input("Done, Press Enter to Continue")
 
 if __name__=="__main__":
     print_message_head("Welcome To Password Locker")
@@ -47,8 +70,12 @@ if __name__=="__main__":
         if option ==1:
             add_existing_account()
 
+        if option == 2:
+            add_new_account()
+
         if option == 4:
             list_credentials()
 
         if option == 5:
+            print("Bye!!")
             break
